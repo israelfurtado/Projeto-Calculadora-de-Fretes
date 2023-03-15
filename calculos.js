ba60e7b20bf12f7500db813df29570bb62f861ee
israelfurtado <israel.furtado@outlook.com.br>
2023-03-15 00:04:10 -0300

    Criação da função calculaFrete.

---
+++ calculos.js
@@ -0,0 +1,4 @@
+const calculaFrete = (produto, endereco) => {
+    console.log(`Olá, boas vindas à nossa Loja. Já recebemos as informações e iremos mandar o produto ${produto} para ${endereco}`)
+}
+
